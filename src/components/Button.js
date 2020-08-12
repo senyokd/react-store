8e318adac68df70a809151c8f84decfac0f4e37b
Senyo Kodjo Dzantor <68573498+senyokd@users.noreply.github.com>
2020-08-12 03:32:39 +0100

    added react store app

---
+++ src/components/Button.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+
+export const ButtonContainer = styled.button`
+text-transform: capitalize; 
+font-size: 1.4rem;
+background: transparent;
+border: 0.05rem solid #009ffd;
+border-color: ${props => props.cart? "#ffa400" : "#009ffd"};
+color:${prop => prop.cart ? "#ffa400" : "#009ffd" };
+border-radius: 0.5rem;
+padding: 0.2rem 0.5rem;
+cursor: pointer;
+margin: 0.2rem 0.5rem 0.2rem 0;
+transition: all 0.5s ease-in-out;
+&:hover{
+    background:${prop => prop.cart ? "yellow" : "slateblue"};
+    colour:#2a2a72;
+}
+&:focus {
+    outline: none;
+}
+`;
